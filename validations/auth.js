473441646b77591e2f8bd3dfbfb7210165f2106d
Marsel <nowly2k@mail.ru>
2023-08-31 19:39:43 +0300

    added auth validation

--- validations/auth.js
+++ validations/auth.js
@@ -1,6 +1,8 @@
 import { body } from "express-validator";
 
-const registerValidator = [
+export const registerValidator = [
     body('email').isEmail(),
     body('password').isLength({ min: 5 }),
+    body('fullName').isLength({ min: 2 }),
+    body('avatarUrl').optional().isURL(),
 ]
