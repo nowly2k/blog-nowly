3874ea5d605d078d608be62e23f905025627b935
Marsel <nowly2k@mail.ru>
2023-08-31 19:50:52 +0300

    auth added to indexjs

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import { validationResult } from "express-validator";
+
+import { registerValidator } from './validations/auth.js'
 
 mongoose
     .connect('mongodb+srv://nowly:6277mars@cluster0.3lrp1om.mongodb.net/?retryWrites=true&w=majority')
@@ -11,8 +14,15 @@ const app = express();
 
 app.use(express.json())
 
-app.post('/auth/register', (req, res) => {
+app.post('/auth/register', registerValidator, (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array())
+    }
 
+    res.json({
+        success: true,
+    })
 })
 
 app.listen(4444, (err) => {
