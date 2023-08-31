69f753019b4509d757fcbdd3d09eba5ee2975600
Marsel <nowly2k@mail.ru>
2023-08-31 20:22:11 +0300

    new auth

--- validations/auth.js
+++ validations/auth.js
@@ -1,8 +1,8 @@
 import { body } from "express-validator";
 
 export const registerValidator = [
-    body('email').isEmail(),
-    body('password').isLength({ min: 5 }),
-    body('fullName').isLength({ min: 2 }),
-    body('avatarUrl').optional().isURL(),
+    body('email', 'Неверный формат почты').isEmail(),
+    body('password', 'Минимальная длина пароля 5 символов').isLength({ min: 5 }),
+    body('fullName', 'Укажите имя').isLength({ min: 2 }),
+    body('avatarUrl', 'Некорректная ссылка на аватарку').optional().isURL(),
 ]
