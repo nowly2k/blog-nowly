73aa8590c64cffda53bb3e616b8919551bcf64f8
Marsel <nowly2k@mail.ru>
2023-09-04 19:35:33 +0300

    get all posts added

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,4 +1,35 @@
-export const create = (req, res) => {
+import PostModel from "../models/Post.js"
+
+export const getAll = async (req, res) => {
     try {
-    } catch (err) { }
+        const posts = await PostModel.find()
+
+        res.json(posts)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Не удалось получить статьи',
+        })
+    }
+}
+
+export const create = async (req, res) => {
+    try {
+        const doc = new PostModel({
+            title: req.body.title,
+            text: req.body.title,
+            imageUrl: req.body.imageUrl,
+            tags: req.body.tags,
+            user: req.userId
+        })
+
+        const post = await doc.save()
+
+        res.json(post)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Не удалось создать статью',
+        })
+    }
 }
--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import express from 'express';
 
 import mongoose from 'mongoose';
 
-import { registerValidator, loginValidator } from './validation.js'
+import { registerValidator, loginValidator, postCreateValidator } from './validation.js'
 
 import checkAuth from './utils/checkAuth.js'
 
 import * as UserController from './controllers/UserController.js'
+import * as PostController from './controllers/PostController.js'
 
 mongoose
     .connect(
@@ -22,6 +23,12 @@ app.use(express.json())
 app.post('/auth/login', loginValidator, UserController.login)
 app.post('/auth/register', registerValidator, UserController.register)
 app.get('/auth/me', checkAuth, UserController.getMe)
+
+app.get('/posts', PostController.getAll)
+// app.get('/posts/:id', PostController.getOne)
+app.post('/posts', checkAuth, postCreateValidator, PostController.create)
+// app.delete('/posts', PostController.remove)
+// app.patch('/posts', PostController.update)
 
 app.listen(4444, (err) => {
     if (err) {
