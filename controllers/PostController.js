c81ff7d4897fb43d16e0aac5b41726067c3a0a83
Marsel <nowly2k@mail.ru>
2023-09-04 19:53:50 +0300

    get one post added

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -2,9 +2,41 @@ import PostModel from "../models/Post.js"
 
 export const getAll = async (req, res) => {
     try {
-        const posts = await PostModel.find()
-
+        const posts = await PostModel.find().populate('user').exec()
         res.json(posts)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Не удалось получить статьи',
+        })
+    }
+}
+
+export const getOne = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        PostModel.findOneAndUpdate(
+            { _id: postId },
+            { $inc: { viewsCount: 1 } },
+            { returnDocument: 'after' }
+        )
+            .then((doc) => {
+                if (!doc) {
+                    return res.status(404).json({
+                        message: 'Статья не найдена'
+                    });
+                }
+
+                res.json(doc);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json({
+                    message: 'Не удалось вернуть статью'
+                });
+            });
+
     } catch (err) {
         console.log(err)
         res.status(500).json({
