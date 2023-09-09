9e5878b0fe63eacc4cb8ffa570b068006ef8d7d0
Marsel <nowly2k@mail.ru>
2023-09-09 21:50:30 +0300

    cors added

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import multer from 'multer';
+import cors from 'cors'
 
 import mongoose from 'mongoose';
 
@@ -30,6 +31,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 app.use(express.json())
+app.use(cors())
 app.use('/uploads', express.static('uploads'))
 
 app.post('/auth/login', loginValidator, handleValidationErrors, UserController.login)
