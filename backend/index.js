9816c919ac68759213ccf895ecce929e2fccc371
Marsel <nowly2k@mail.ru>
2023-09-09 22:38:55 +0300

    .

--- backend/index.js
+++ backend/index.js
@@ -45,6 +45,7 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
 })
 
 app.get('/posts', PostController.getAll)
+app.get('/posts/tags', PostController.getLastTags)
 app.get('/posts/:id', PostController.getOne)
 app.post('/posts', checkAuth, postCreateValidator, handleValidationErrors, PostController.create)
 app.delete('/posts/:id', checkAuth, PostController.remove)
