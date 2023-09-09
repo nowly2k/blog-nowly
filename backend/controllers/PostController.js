e63e3ee8b50ea970fa6e00ed7e81d6c6f75da5be
Marsel <nowly2k@mail.ru>
2023-09-09 22:45:38 +0300

    .

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -3,15 +3,12 @@ import PostModel from "../models/Post.js"
 export const getLastTags = async (req, res) => {
     try {
         const posts = await PostModel.find().limit(5).exec()
-
         const tags = posts.map(obj => obj.tags).flat().slice(0, 5)
-
-
-        res.json(posts)
+        res.json(tags)
     } catch (err) {
         console.log(err)
         res.status(500).json({
-            message: 'Не удалось получить статьи',
+            message: 'Не удалось получить теги',
         })
     }
 }
