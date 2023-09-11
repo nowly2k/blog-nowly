8bc880c16ba7d0073317f58ed15708c49085bd20
Marsel <nowly2k@mail.ru>
2023-09-11 12:38:51 +0300

    bug fix

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -93,7 +93,7 @@ export const create = async (req, res) => {
     try {
         const doc = new PostModel({
             title: req.body.title,
-            text: req.body.title,
+            text: req.body.text,
             imageUrl: req.body.imageUrl,
             tags: req.body.tags,
             user: req.userId
